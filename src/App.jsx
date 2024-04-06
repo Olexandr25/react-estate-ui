aa807d31f33e9392ca2274844557d0cd1a1b57e3
Olexandr25 <oleksandrvoichyshyn@gmail.com>
2024-04-06 18:21:03 +0300

    Add Navbar component and styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
 import './layout.scss'
+import { Navbar } from './components'
 
 function App() {
-  return <div className='layout'>Hello World</div>
+  return (
+    <div className='layout'>
+      <Navbar />
+      {/* <div>Hello World</div> */}
+    </div>
+  )
 }
 
 export default App
