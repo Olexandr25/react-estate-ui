4ce78d1f46a1bdd6aa656a99f94c82653034344d
Olexandr25 <oleksandrvoichyshyn@gmail.com>
2024-04-07 10:25:02 +0300

    Add Filter and Card components

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,5 @@
 export { default as Navbar } from './Navbar/Navbar.jsx'
 export { default as SearchBar } from './SearchBar/SearchBar.jsx'
 export { default as Layout } from './Layout/Layout.jsx'
+export { default as Filter } from './Filter/Filter.jsx'
+export { default as Card } from './Card/Card.jsx'
