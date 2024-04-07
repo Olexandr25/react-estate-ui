e1c2ef2fdb29ed0e5028ca49263698159e5655e2
Olexandr25 <oleksandrvoichyshyn@gmail.com>
2024-04-07 10:25:21 +0300

    Update layout and navbar styles

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,35 +1,36 @@
 import './navbar.scss'
 import { useState } from 'react'
+import { Link } from 'react-router-dom'
 
 const Navbar = () => {
   const [open, setOpen] = useState(false)
   return (
     <nav>
       <div className='left'>
-        <a href='/' className='logo'>
+        <Link href='/' className='logo'>
           <img src='/logo.png' alt='' />
           <span>LamaEstate</span>
-        </a>
-        <a href='/'>Home</a>
-        <a href='/'>About</a>
-        <a href='/'>Contact</a>
-        <a href='/'>Agents</a>
+        </Link>
+        <Link href='/'>Home</Link>
+        <Link href='/'>About</Link>
+        <Link href='/'>Contact</Link>
+        <Link href='/'>Agents</Link>
       </div>
       <div className='right'>
-        <a href='/'>Sign in</a>
-        <a href='/' className='register'>
+        <Link href='/'>Sign in</Link>
+        <Link href='/' className='register'>
           Sign up
-        </a>
+        </Link>
         <div className='menuIcon'>
           <img src='/menu.png' alt='' onClick={() => setOpen(prev => !prev)} />
         </div>
         <div className={open ? 'menu active' : 'menu'}>
-          <a href='/'>Home</a>
-          <a href='/'>About</a>
-          <a href='/'>Contact</a>
-          <a href='/'>Agents</a>
-          <a href='/'>Sign in</a>
-          <a href='/'>Sign up</a>
+          <Link href='/'>Home</Link>
+          <Link href='/'>About</Link>
+          <Link href='/'>Contact</Link>
+          <Link href='/'>Agents</Link>
+          <Link href='/'>Sign in</Link>
+          <Link href='/'>Sign up</Link>
         </div>
       </div>
     </nav>
