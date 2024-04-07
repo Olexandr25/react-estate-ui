0fbbc3b0fd93517ce3ad22b36e991ebc27af4e8c
Olexandr25 <oleksandrvoichyshyn@gmail.com>
2024-04-07 10:25:16 +0300

    Add dummy data for list and single post, and user data

--- src/routes/ListPage/ListPage.jsx
+++ src/routes/ListPage/ListPage.jsx
@@ -1,7 +1,21 @@
+import { Card, Filter } from '../../components'
 import './ListPage.scss'
+import { listData } from '../../lib/dummydata'
 
 const ListPage = () => {
-  return <div className='listPage'>ListPage</div>
+  return (
+    <div className='listPage'>
+      <div className='listContainer'>
+        <div className='wrapper'>
+          <Filter />
+          {listData?.map(data => (
+            <Card key={data.id} data={data} />
+          ))}
+        </div>
+      </div>
+      <div className='mapContainer'>Map</div>
+    </div>
+  )
 }
 
 export default ListPage
